556f3b14171913b95d41031c5cfba8a1c5634078
Ryan Montgomery <ryanmontgomery@nicoles-mbp.myfiosgateway.com>
2020-11-09 21:32:51 -0500

    Update db config

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ const config = {
 if(env == 'development'){
 
 }else{
-    config.DATABASE = 'aa1qqw435zdt1ld'
-    config.PASSWORD = 'bd12_efi56',
-    config.USERNAME = 'disciplineDev',
+    config.DATABASE = 'd7q5tufv8n8h7p'
+    config.PASSWORD = '590453d3ecb0a321fc1d57f0b708891b9195f64424b084c640f80c362e3f0e94',
+    config.USERNAME = 'mmqxsypeshmgpa',
     config.DB_PORT = '5432',
-    config.HOST = 'aa1qqw435zdt1ld.cmg9iliyxleb.us-east-1.rds.amazonaws.com',
+    config.HOST = 'ec2-52-207-124-89.compute-1.amazonaws.com',
     config.DIALECT = 'postgres'
 }
 
