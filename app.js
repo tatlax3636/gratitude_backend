9340218a92f4528347132de7f54bcd05d4009733
Ryan Montgomery <ryanmontgomery@nicoles-mbp.myfiosgateway.com>
2020-11-09 21:28:44 -0500

    remove listener

--- app.js
+++ app.js
@@ -46,9 +46,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+
 
 
 module.exports = app;
--- scripts/start.sh
+++
@@ -1,5 +0,0 @@
-npm install
-npx sequelize-cli db:migrate:undo:all --config config/dbConfig.js
-npx sequelize-cli db:migrate --config config/dbConfig.js
-npx sequelize-cli db:seed:all --config config/dbConfig.js
-node ./bin/www
