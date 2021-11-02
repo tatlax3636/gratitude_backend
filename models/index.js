99d871cad7004d5c57016a190cc833dfd07c966b
Ryan Montgomery <ryanmontgomery@nicoles-mbp.myfiosgateway.com>
2021-11-02 13:57:34 -0400

    use other db imp

--- models/index.js
+++ models/index.js
@@ -11,8 +11,16 @@ const db = {};
 
 //let  sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-let sequelize = new Sequelize(`${config.DB_URL}?sslmode=require`) 
-
+//let sequelize = new Sequelize(`${config.DB_URL}?sslmode=require`) 
+let sequelize = new Sequelize(Config.DB_URL, {
+  dialect: `postgres`,
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  }
+})
 
 
 
