69aad7d60b8a927e2aa4c46a9d662baaf45a1239
unknown <ryanmont@udel.edu>
2023-11-02 19:39:38 -0400

    Remove year filter

--- controllers/leafController.js
+++ controllers/leafController.js
@@ -32,7 +32,7 @@ const getLeaves = async (req, res) => {
         console.log(data)
         let leaves = data.map(leaf => leaf.dataValues)
         console.log(leaves[0].createdAt.toString().split(' '))
-        leaves = leaves.filter(leaf => (leaf.createdAt).toString().split(' ')[3] == '2022')
+        // leaves = leaves.filter(leaf => (leaf.createdAt).toString().split(' ')[3] == '2023')
         res.status(200).send(leaves)
     }).catch(err => {
         console.log(Sequelize);
