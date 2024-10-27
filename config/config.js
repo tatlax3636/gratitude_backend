047fdbd913575b692e7bfec0d948dca640b8a03f
Ryan Montgomery <ryanmont@udel.edu>
2024-10-27 18:56:54 -0400

    Update db for 2024

--- config/config.js
+++ config/config.js
@@ -6,19 +6,19 @@ const config = {
     PASSWORD: '',
     HOST: '127.0.0.1',
     DIALECT: 'postgres',
-    DB_URL: 'postgres://mmqxsypeshmgpa:590453d3ecb0a321fc1d57f0b708891b9195f64424b084c640f80c362e3f0e94@ec2-52-207-124-89.compute-1.amazonaws.com:5432/d7q5tufv8n8h7p'
+    DB_URL = 'postgres://u35d7q95pstf5g:p20cffd739176323a0be33ebab8dd49e45a031b73e0865ed715a0f816cceea825@c1i13pt05ja4ag.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d23biaoe2mmot8',
 }
 
 if(env == 'development'){
 
 }else{
-    config.DATABASE = 'd7q5tufv8n8h7p'
-    config.PASSWORD = '590453d3ecb0a321fc1d57f0b708891b9195f64424b084c640f80c362e3f0e94',
-    config.USERNAME = 'mmqxsypeshmgpa',
+    config.DATABASE = 'd23biaoe2mmot8'
+    config.PASSWORD = 'p20cffd739176323a0be33ebab8dd49e45a031b73e0865ed715a0f816cceea825',
+    config.USERNAME = 'u35d7q95pstf5g',
     config.DB_PORT = '5432',
-    config.HOST = 'ec2-52-207-124-89.compute-1.amazonaws.com',
+    config.HOST = 'c1i13pt05ja4ag.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
     config.DIALECT = 'postgres',
-    config.DB_URL = 'postgres://mmqxsypeshmgpa:590453d3ecb0a321fc1d57f0b708891b9195f64424b084c640f80c362e3f0e94@ec2-52-207-124-89.compute-1.amazonaws.com:5432/d7q5tufv8n8h7p',
+    config.DB_URL = 'postgres://u35d7q95pstf5g:p20cffd739176323a0be33ebab8dd49e45a031b73e0865ed715a0f816cceea825@c1i13pt05ja4ag.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d23biaoe2mmot8',
     config.DIALECT_OPTIONS = {
         ssl: true,
         rejectUnauthorized: false
